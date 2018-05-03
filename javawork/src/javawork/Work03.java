201b3409284611f5bbc6679e5157ee9bc1ba8566
nmxx <617882414@qq.com>
2018-05-03 16:36:50 +0800

    2018.5.3

---
+++ javawork/src/javawork/Work03.java
@@ -0,0 +1,14 @@
+package javawork;
+
+public class Work03 {
+	public static void main(String[] args) {
+		int number=0;
+		
+		while(number<=100){
+		if(number%2==0) {
+		System.out.println(number);
+		}	
+		++number;
+		}
+	}
+}
